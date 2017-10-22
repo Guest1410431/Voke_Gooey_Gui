2937cd02a788baced6043b4c40037f484aa26427
Ania Kaczka Jennings <rakaczkaje42@students.tntech.edu>
2017-10-22 10:57:17 -0500

    added room for better api calls

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -59,33 +59,6 @@ function scrollContent(){
     }, 300);
 }
 
-function getTagId(nameInputed){    
-    $.ajax({
-        type: "GET",
-        url: "https://api-stage.vokeapp.com/api/messenger/v1/tags",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        headers: {
-            "Authorization": "Bearer " + "58098d33482e25580301c04271347713c98699daeeb9d91f32d5299e13f0f697"
-        },
-        success: function(data){
-            var tagList = data.tags;
-            
-            for(var i=0; i<tagList.length; i++){
-                if(tagList[i].name === nameInputed){
-                    return tagList[i].id;
-                }
-            }
-            return -1;
-        },
-        error: function(xhr, status, error) {
-                var err = eval("(" + xhr.responseText + ")");
-                console.log(err);
-                console.log(status);
-                console.log(error);
-        }
-    });
-}
 function getEmbededVideo(videoURL){
     return '<iframe width="150" height="100" src='+ videoURL.replace("watch?v=", "embed/") +' frameborder="0" allowfullscreen></iframe>';
 }
@@ -99,31 +72,47 @@ function getBotResponse(input){
     var vidList = [];
     
     $.ajax({
-                type: "POST",
-                url: baseUrl + "query?v=20150910",
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                headers: {
-                    "Authorization": "Bearer " + accessToken
-                },
-                data: JSON.stringify({ query: input, lang: "en", sessionId: "somerandomthing" }),
-                success: function(data) {
-                    botAns = data.result.fulfillment.speech;
+        type: "POST",
+        url: baseUrl + "query?v=20150910",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers: {
+            "Authorization": "Bearer " + accessToken
+        },
+        data: JSON.stringify({ query: input, lang: "en", sessionId: "somerandomthing" }),
+        success: function(data) {
+            botAns = data.result.fulfillment.speech;
+            
+            console.log(data);
+            if (data.result.action === "video.play"){
+                if(data.result.parameters.tag.length > 0){
                     var tag = data.result.parameters.tag;
-
-                    getVideoFromTag(tag);
-                    addChatMessage(botAns, false);
-                },
-                error: function(xhr, status, error) {
-                    var err = eval("(" + xhr.responseText + ")");
-                    console.log(err);
-                    console.log(status);
-                    console.log(error);
+                    getTagId(tag);
                 }
-            }); 
+                else if(data.result.parameters.organization.length > 0){
+                    
+                }
+                else if(data.result.parameters.item.length > 0){
+
+                }        
+
+                
+            }
+            
+            addChatMessage(botAns, false);
+        },
+        error: function(xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            console.log(err);
+            console.log(status);
+            console.log(error);
+        }
+    }); 
 }
 
 function getVideoFromTag(tag){
+    //var tagID = getTagId(tag);
+    //console.log(tag);
     $.ajax({
         type: "GET",
         url: "https://api-stage.vokeapp.com/api/messenger/v1/items",
@@ -133,10 +122,9 @@ function getVideoFromTag(tag){
             "Authorization": "Bearer " + "58098d33482e25580301c04271347713c98699daeeb9d91f32d5299e13f0f697"
         },
         data: {  
-            tag_id: getTagId(tag)
+            tag_id: tag
         },
         success: function(data){
-                console.log(data.items.length);
                 vidList = data;       
                 addChatMessage(getEmbededVideo(vidList.items[0].media.url),false);
         },
@@ -147,4 +135,37 @@ function getVideoFromTag(tag){
                 console.log(error);
             }
     });
+}
+
+function getTagId(nameInputed){    
+    $.ajax({
+        type: "GET",
+        url: "https://api-stage.vokeapp.com/api/messenger/v1/tags",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers: {
+            "Authorization": "Bearer " + "58098d33482e25580301c04271347713c98699daeeb9d91f32d5299e13f0f697"
+        },
+        success: function(data){
+            var tagList = data.tags;
+            console.log(data.tags);
+            
+            for(var i=0; i<tagList.length; i++){
+                if(tagList[i].name === nameInputed){
+                    getVideoFromTag(tagList[i].id);
+                    return;
+                }
+            }
+
+
+            return -1;
+        },
+        error: function(xhr, status, error) {
+                var err = eval("(" + xhr.responseText + ")");
+                console.log(err);
+                console.log(status);
+                console.log(error);
+        }
+    });
+
 }
