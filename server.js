ae4618ce71f7eb5056bd62377a2217185231719a
Ania Kaczka Jennings <rakaczkaje42@students.tntech.edu>
2017-10-22 09:19:01 -0500

    repo cleanup

--- server.js
+++ server.js
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var bodyParser = require('body-parser')
-var expressValidator = require('express-validator');
-
-var session;
-var app = express();
-
-app.use( bodyParser.json() );
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(expressValidator()); 
-
-app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + "/public"));
-app.use('/assets', express.static(__dirname + "/public"));
-
-//Page links
-app.get('/', function(request, response){
-    response.render('index');   
-});
-
-app.post('/sendMessage', function(request, response){
-    request.checkBody('message', '').notEmpty();
-    request.sanitizeBody('message').escape();
-    
-    if(!request.validationErrors()){
-        var message_string = request.body.message;
-        console.log('message: ' + request.body.message);
-    }
-});
-app.listen(8080, '0.0.0.0', function(){
-    console.log("Connected successfully to port 8080");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
 var express = require('express');
 var bodyParser = require('body-parser')
 var expressValidator = require('express-validator');
@@ -93,21 +41,3 @@ app.post('/sendMessage', function(request, response){
 app.listen(8080, '0.0.0.0', function(){
     console.log("Connected successfully to port 8080");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> fc7fd629c97d3d4f68d3e8465a0fb2b792954f50
